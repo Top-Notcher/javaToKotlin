57e99f09720a20e81da6a3ede2fc071ca2b97fe8
박현민 <dndb4902@naver.com>
2023-07-05 16:46:50 +0900

    #1. 코틀린에서 변수를 다루는 방법

---
+++ src/main/java/com/lannstark/lec01/Lec01Main.kt
@@ -0,0 +1,18 @@
+package com.lannstark.lec01
+
+fun main() {
+    var number1: Long = 10L      // 바꿀 수 있는 변수
+    val number2: Long = 10L      // 바꿀 수 없는 변수
+
+    var number3: Long
+//    println(number3)  // complile error
+
+    val number4: Long
+    number4 = 10L
+
+
+    var number5: Long? = 1_000L
+    number5 = null
+
+    var person = Person("박현민")
+}
