a7aac7aa742daed327050a023af0343be6df8b02
박현민 <dndb4902@naver.com>
2023-07-06 13:43:57 +0900

    #4. 코틀린에서 연산자를 다루는 방법

---
+++ src/main/java/com/lannstark/lec04/Lec04Main.java
@@ -0,0 +1,21 @@
+package com.lannstark.lec04;
+
+public class Lec04Main {
+
+    public static void main(String[] args) {
+        JavaMoney money1 = new JavaMoney(1_000L);
+        JavaMoney copyMoney = money1;
+        JavaMoney money2 = new JavaMoney(2_000L);
+
+        if(money1.compareTo(money2) > 0) {
+            System.out.println("Money1이 Money2보다 금액이 크다.");
+        }
+
+        System.out.println(money1 == copyMoney);    // true
+        System.out.println(money1 == money2);       // false
+        System.out.println(money1.equals(money2));  // true
+
+        System.out.println(money1.plus(money2));
+    }
+
+}
