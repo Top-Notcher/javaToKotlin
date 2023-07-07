b9d385d0afea21c347836a7e8f6c75ed33d16006
박현민 <dndb4902@naver.com>
2023-07-07 18:12:44 +0900

    #7. 코틀린에서 예외를 다루는 방법

---
+++ src/main/java/com/lannstark/lec07/JavaFilePrinter.java
@@ -0,0 +1,26 @@
+package com.lannstark.lec07;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JavaFilePrinter {
+
+    //3. 프로젝트 내 파일의 내용물을 읽어오는 예제
+    public void readFile() throws IOException {
+        File currentFile = new File(".");
+        File file = new File(currentFile.getAbsolutePath() + "/a.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        System.out.println(reader.readLine());
+        reader.close();
+    }
+
+    // 4. 프로젝트 내 파일의 내용물을 읽어오는 예제
+   public void readFile(String path) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            System.out.println(reader.readLine());
+        }
+    }
+
+}
