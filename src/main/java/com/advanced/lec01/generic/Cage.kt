a1dca44ea83cdc638a0e0ad802e3544afc70aa8f
minit <dndb4902@naver.com>
2024-10-18 10:02:28 +0900

    # 코틀린 고급편 1강. 제네릭과 타입 파라미터

--- src/main/java/com/advanced/lec01/generic/Cage.kt
+++ src/main/java/com/advanced/lec01/generic/Cage.kt
@@ -1,33 +1,28 @@
 package com.advanced.lec01.generic
 
 
-fun main() {
+fun example01() {
     /**
      * example01
      */
     val cage = Cage()
     cage.put(Carp("잉어"))
-//    val carp: Carp = cage.getFirst()              // Error: Type Mismatch
-//    val carp: Carp = cage.getFirst() as Carp      // 만약에 넣은게 금붕어였다면?! -> 런타임 에러
 
-    // Safe Type Casting과 Elvis Operator
-    val carp: Carp = cage.getFirst() as? Carp?: throw  IllegalArgumentException()
+    // 1. Error: Type Mismatch -> 컴파일 에러
+//    val carp1: Carp = cage.getFirst()
 
+    // 2. 만약에 넣은게 금붕어였다면?! -> 런타임 에러
+    val carp2: Carp = cage.getFirst() as Carp
 
-    // generic 활용
+    // 3. Safe Type Casting과 Elvis Operator -> Exception 이 발생할 수 있다!
+    val carp3: Carp = cage.getFirst() as? Carp?: throw  IllegalArgumentException()
+
+    // 4. generic 활용
     val cage2 = Cage2<Carp>()
     cage2.put(Carp("잉어"))
-    val carp2: Carp = cage2.getFirst()
-
-    /**
-     * example02
-     */
-    val goldFishCage = Cage2<GoldFish>()
-    goldFishCage.put(GoldFish("금붕어"))
-
-    val fishCage = Cage2<Fish>()
-//    fishCage.moveFrom(goldFishCage) // Type Mismatch
+    val carp4: Carp = cage2.getFirst()
 }
+
 
 class Cage {
     private val animals: MutableList<Animal> = mutableListOf()
