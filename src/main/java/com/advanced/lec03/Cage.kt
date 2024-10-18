e0c9dd12ff77df5a2210d017c24c5de5438bfec8
minit <dndb4902@naver.com>
2024-10-18 14:22:37 +0900

    # 코틀린 고급편 3강. 공변과 반공변

--- src/main/java/com/advanced/lec03/Cage.kt
+++ src/main/java/com/advanced/lec03/Cage.kt
@@ -1,27 +1,31 @@
 package com.advanced.lec03
 
 
-fun main() {
+fun example02() {
     /**
-     * example02
+     * example02 out 키워드 : 공변하게 만들기!
      */
     val goldFishCage = Cage2<GoldFish>()
     goldFishCage.put(GoldFish("금붕어"))
 
-    val fishCage = Cage2<Fish>()
-    fishCage.moveFrom(goldFishCage)
+    val fishCage1 = Cage2<Fish>()
+    fishCage1.moveFrom(goldFishCage)
 
-    val first: Fish = fishCage.getFirst()
+    val first: Fish = fishCage1.getFirst()
 
+
+}
+
+fun example03() {
     /**
-     * example03
+     * example03 in 키워드 : 반공변하게 만들기!
      */
     val fishCage2 = Cage2<Fish>()
 
     val goldFishCage2 = Cage2<GoldFish>()
     goldFishCage2.put(GoldFish("금붕어"))
     goldFishCage2.moveTo(fishCage2)     // Type Mismatch - Cage2<Fish>가 Cage2<GoldFish>의 하위 타입이어야 한다.
-                                        // Cage2를 moveTo 함수에서 반공변하게 만들어야한다!
+    // Cage2를 moveTo 함수에서 반공변하게 만들어야한다!
 }
 
 class Cage2<T> {
@@ -37,7 +41,10 @@ class Cage2<T> {
 
     fun moveFrom(otherCage: Cage2<out T>) {
         otherCage.getFirst()
-//        otherCage.put("test") // error! because out
+
+        // error! -> out 키워드를 붙이면 데이터 읽기만 가능하다.
+//        otherCage.put("test")
+
         this.animals.addAll(otherCage.animals)
     }
 
